4d8c6ada6fe7c7d3412ff3400321acf5bfcbe77e
komalkushwaha30 <komalkuswaha30@gmail.com>
2025-07-05 14:45:39 +0530

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // Use '/' for root deployment; adjust if deploying to a subpath
-  plugins: [react()],
+  base: '/',
+  plugins: [react()]
 });
